9dc6dd52103a64d647a3b1a31372359a913d9372
Illia Shatokhin <resonane.2106@gmail.com>
2023-05-05 16:07:08 +0300

    add styles and comments

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,6 +1,8 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
+// Declaration of constants and variables
 const dateInput = document.getElementById('datetime-picker');
 const startButton = document.querySelector('button');
 const valueSpans = document.querySelectorAll('.value');
@@ -12,7 +14,7 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     if (selectedDates[0].getTime() <= new Date().getTime()) {
-      alert('Please choose a date in the future');
+      Notify.failure('Please choose a date in the future');
     } else {
       startButton.removeAttribute('disabled', 'disabled');
       startButton.setAttribute('enabled', 'enabled');
@@ -20,23 +22,45 @@ const options = {
     }
   },
 };
-
 let dateInputValue = 0;
-let a = 1001;
+let minMs = 1001;
 let intervalId = 0;
 
 flatpickr(dateInput, options);
 
 startButton.setAttribute('disabled', 'disabled');
 
+//Add styles
+document.querySelector('.timer').style.display = 'flex';
+document.querySelector('.timer').style.gap = '10px';
+valueSpans.forEach(el => {
+  el.style.display = 'block';
+  el.style.fontWeight = '700';
+  el.style.fontSize = '42px';
+  el.style.lineHeight = '57px';
+  el.style.textAlign = 'center';
+  el.style.letterSpacing = '0.05em';
+  el.style.color = '#303030';
+});
+document.querySelectorAll('.label').forEach(el => {
+  el.style.fontWeight = '700';
+  el.style.fontSize = '15px';
+  el.style.lineHeight = '18px';
+  el.style.textAlign = 'center';
+  el.style.color = '#303030';
+});
+
+// Add listener on start button with setInterval
 startButton.addEventListener('click', () => {
   countdown();
   intervalId = setInterval(countdown, 1000);
 });
 
+// Functions
 function countdown() {
-  if (a <= 1000) {
+  if (minMs <= 1000) {
     clearInterval(intervalId);
+    Notify.success('The time has come. Congratulations');
   } else {
     const valuesСonvertMs = Object.values(
       convertMs(computeMsDifference(new Date(), dateInputValue))
@@ -46,7 +70,7 @@ function countdown() {
 
       changeTextcontent(el, addLeadingZero(valueConvertMs));
     });
-    a = computeMsDifference(new Date(), dateInputValue);
+    minMs = computeMsDifference(new Date(), dateInputValue);
   }
 }
 
